18797301cd1564117149b92c6d64b265cba851af
wanglang <wanglang@1862.cn>
2016-12-18 16:32:52 +0800

    Site updated: 2016-12-18 16:32:52

--- js/totop.js
+++ js/totop.js
@@ -11,7 +11,10 @@
 
 	// Show and hide the scroll to top link based on scroll position
 	scrollElem.hide();
-	$(window).scroll(function () {
+    
+    //mobile
+	$(document).scroll(function () {
+        console.log("ddddd");
 		var scrollTop = $(document).scrollTop();
 		if ( scrollTop > upperLimit ) {
 			$(scrollElem).stop().fadeTo(300, 1); // fade back in
@@ -20,8 +23,49 @@
 		}
 	});
 
+    //pc
+    $('#container').scroll(function () {
+        console.log("dddddcontainer");
+		var scrollTop = $('#container').scrollTop();
+		if ( scrollTop > upperLimit ) {
+			$(scrollElem).stop().fadeTo(300, 1); // fade back in
+		}else{
+			$(scrollElem).stop().fadeTo(300, 0); // fade out
+		}
+	});
+       
 	// Scroll to top animation on click
 	$(scrollElem).click(function(){
-		$('html, body').animate({scrollTop:0}, scrollSpeed); return false;
+		$('html, body').animate({scrollTop:0}, scrollSpeed); 
+        $('#container').animate({scrollTop:0}, scrollSpeed); return false;
 	});
+         
+    
 })(jQuery);
+
+$(function() {
+	var Accordion = function(el, multiple) {
+		this.el = el || {};
+		this.multiple = multiple || false;
+
+		// Variables privadas
+		var links = this.el.find('.link');
+		// Evento
+		links.on('click', {el: this.el, multiple: this.multiple}, this.dropdown)
+	}
+
+	Accordion.prototype.dropdown = function(e) {
+		var $el = e.data.el;
+			$this = $(this),
+			$next = $this.next();
+
+		$next.slideToggle();
+		$this.parent().toggleClass('open');
+
+		if (!e.data.multiple) {
+			$el.find('.submenu').not($next).slideUp().parent().removeClass('open');
+		};
+	}	
+
+	var accordion = new Accordion($('#accordion'), false);
+});
